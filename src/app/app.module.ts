02d4b5fa8e84592b5e1a0afbfc1f71ee7391dd9c
Gustav <gust1224@live.dk>
2020-03-09 14:11:46 +0100

    Opsætning af angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { GalgelegComponent } from './galgeleg/galgeleg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    GalgelegComponent
   ],
   imports: [
     BrowserModule
